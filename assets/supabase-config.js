031252548c0775ae78213a7dfa2a5e93b26200f1
Hashan soysa <hashansoysa92@gmail.com>
2026-09-16 00:02:28 +0500

    Add Supabase CMS configuration

---
+++ assets/supabase-config.js
@@ -0,0 +1,6 @@
+window.HASHAN_CMS = {
+  supabaseUrl: 'https://caxbhzgmwkhgdblotjvs.supabase.co',
+  supabaseKey: 'sb_publishable_p6qXdrM6TF0ZW7Z5btj5jQ_-WP-ou6N',
+  table: 'hashansoysa_portfolio_content',
+  rowId: 'site'
+};
