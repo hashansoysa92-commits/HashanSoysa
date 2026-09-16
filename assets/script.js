310e4f879f6d898abd9cb66e7926f6c4bfa66cc3
Hashan soysa <hashansoysa92@gmail.com>
2026-09-16 13:48:43 +0500

    Open Drive photos and albums inside the site

--- assets/script.js
+++ assets/script.js
@@ -108,5 +108,5 @@
   }
 
   const loadScript = src => new Promise((resolve,reject)=>{const s=document.createElement('script');s.src=src;s.onload=resolve;s.onerror=reject;document.body.appendChild(s);});
-  loadScript('assets/supabase-config.js?v=20260916.5').then(()=>loadScript('assets/cms.js?v=20260916.5')).catch(()=>{});
+  loadScript('assets/supabase-config.js?v=20260916.9').then(()=>loadScript('assets/cms.js?v=20260916.9')).catch(()=>{});
 })();
