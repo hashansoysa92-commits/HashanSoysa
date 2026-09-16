a608269b5084f8fc3bfae366d21961cdeb333c81
Hashan soysa <hashansoysa92@gmail.com>
2026-09-16 13:48:41 +0500

    Open Drive photos and albums inside the site

--- assets/cms.js
+++ assets/cms.js
@@ -1,1 +1,1 @@
-(() => { const s=document.createElement('script'); s.src='assets/cms-v3.js?v=20260916.5'; document.body.appendChild(s); })();
+(() => { const s=document.createElement('script'); s.src='assets/cms-v3.js?v=20260916.9'; document.body.appendChild(s); })();
