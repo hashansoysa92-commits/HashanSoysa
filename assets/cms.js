849750e3b4edb1a5014f8a3f7460ea4e029ecb6a
Hashan soysa <hashansoysa92@gmail.com>
2026-09-16 16:32:47 +0500

    Restore reliable Drive thumbnails with fallback

--- assets/cms.js
+++ assets/cms.js
@@ -1,1 +1,1 @@
-(() => { const s=document.createElement('script'); s.src='assets/cms-v3.js?v=20260916.11'; document.body.appendChild(s); })();
+(() => { const s=document.createElement('script'); s.src='assets/cms-v3.js?v=20260916.12'; document.body.appendChild(s); })();
